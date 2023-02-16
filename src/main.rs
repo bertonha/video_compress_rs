db852b3d7a0fe95bb010eda9aca267f4aacd182d
Christofer Bertonha <christoferbertonha@gmail.com>
2023-02-16 17:31:37 +0100

    only process non compressed files

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,6 @@ struct Args {
 }
 
 fn call_ffmpeg(in_filename: &Path, out_filename: &Path) {
-    println!("Processing: {}", in_filename.display());
     let mut ffmpeg = Command::new("ffmpeg")
         .arg("-hide_banner")
         .arg("-i")
@@ -47,13 +46,20 @@ fn main() {
         .into_iter()
         .filter_map(|e| e.ok())
         .filter(|e| e.file_type().is_file())
-        .filter(|e| e.path().extension() == Some(args.extension.as_ref()));
+        .filter(|e| e.path().extension() == Some(args.extension.as_ref()))
+        .filter(|e| !e.file_name().to_string_lossy().ends_with(OUTPUT_EXTENSION));
 
     for entry in walker {
         let in_filename = entry.path();
         let mut out_filename = PathBuf::from(in_filename);
         out_filename.set_extension(OUTPUT_EXTENSION);
 
+        if out_filename.exists() {
+            println!("Skipping: {}", in_filename.display());
+            continue;
+        }
+
+        println!("Processing: {}", in_filename.display());
         call_ffmpeg(in_filename, out_filename.as_path());
     }
 }
